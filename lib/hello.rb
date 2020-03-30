616f21f79691d8f95de0d4846a105dd4dc61fbfa
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-03-30 23:07:21 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,13 @@
 def hello_t(array)
+  if block_given?
   i = 0
-  
-  while i < array.length
-    yield array[i]
-    i = i + 1
-  end
-  array
+    while i < array.length
+      yield array[i]
+      i = i + 1
+    end
+    array
+  else
+    
 end
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
